91bce8cd86270d28e9462b7f607bf6c824febcf9
Ally Prof <allyprof@example.com>
2026-06-15 23:07:59 +0300

    feat: landing page image adjustments
    - Resize About section image to maintain 4:3 aspect ratio
    - Add new 8k premium background image for the hero section

--- resources/views/landing/partials/hero.blade.php
+++ resources/views/landing/partials/hero.blade.php
@@ -1,5 +1,5 @@
 <section id="hero" class="hero section dark-background">
-  <img src="{{ asset('gp-assets/img/hero-bg.jpg') }}" alt="" data-aos="fade-in">
+  <img src="{{ asset('gp-assets/img/hero-bg.png') }}" alt="" data-aos="fade-in">
 
   <div class="container">
     <div class="row justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
